a96070f679272a81e65c1bf8e9744d4aa87f96d7
Wai Yan Kyaw <waiyankyawsdk1999@gmail.com>
2022-05-08 15:15:16 +0630

    session2_basics.py

---
+++ session2_basics.py
@@ -0,0 +1,23 @@
+first_num = 10
+
+second_num = 20
+
+addtion = first_num + second_num
+
+print(addtion)
+
+# variable declaration
+# 1 variable name should start with lowercase char
+# 2 digits are allowed but not at the start
+# 3 _ allowed
+
+# id (memory allocation in python)
+# type
+#
+
+print(type(first_num),type(second_num))
+print(id(first_num),id(second_num))
+
+a = 10
+b = 10
+print(id(a),id(b))
