770543986288bb8b98d3170f9d29c1911635f6c8
Wai Yan Kyaw <waiyankyawsdk1999@gmail.com>
2022-05-14 21:46:03 +0630

    session7_list3.py

---
+++ session7_list3.py
@@ -0,0 +1,48 @@
+li = [10,20,30,40,50]
+li[2] = 300
+print(li)
+
+#pop
+#remove
+#clear
+#del
+
+r = li.pop()
+print(li,r)
+r = li.pop(1)
+print(li,r)
+
+li.remove(300)
+print(li)
+#li.remove(20) #not in list
+
+li.clear()
+print(li)
+
+del li
+# print(li)
+
+#sort
+#reverse
+ls = [50,40,30,20,10]
+ls.sort()
+print(ls)
+# print(ls[::-1])
+ls.reverse()
+print(ls)
+
+l3 = sorted(ls)
+print(l3)
+
+#index
+#count
+print(ls.index(30))
+print(ls.count(30))
+
+#list concatenation
+print(ls + l3)
+
+li = [0.1]
+print(li*15)
+
+
